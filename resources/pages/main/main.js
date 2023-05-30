50fbc64af86498ed304dbd784026934a807dd316
Kvdl <evkachmala@gmail.com>
2023-05-30 22:22:48 +0700

    test

---
+++ resources/pages/main/main.js
@@ -0,0 +1,1 @@
+const bg = "/media/bg0.png"
--- src/App.java
+++ src/App.java
@@ -1,7 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(1624);
 
+        final ArrayList<String> IMAGES_EXTENSIONS = new ArrayList<String>(Arrays.asList("webm", "png", "jpeg", "jpg"));
+
+        Server server = new Server(7070);
+        DataOperator.projectPath+="resources/";
+
+        // Main page
+        server.addRequestHandler("", false, (String res, String ip, String req) -> {
+            server.sendResponse(DataOperator.buildPage("pages/main"));
+
+            return true;
+        });
         
+        // Media files
+        server.addRequestHandler("media/", true, (String res, String ip, String request) -> {
+            String filepath = "media/"+res.substring(5);
+            String contentType = "text";
+            String ext = filepath.substring(filepath.lastIndexOf(".")+1);
+            if (ext.equals("mp4")) {contentType="video";}
+            else if (IMAGES_EXTENSIONS.contains(ext)) {contentType="image";}
+                if (DataOperator.isFileExist(filepath)) {
+                    if (DataOperator.getFileSize(filepath)<10500000) {
+                        server.sendResponse(contentType+"/"+ext, DataOperator.readFileAsBytes(filepath));
+                    } else {
+                        server.sendResponseAsync(contentType+"/"+ext, filepath);
+                    }
+                } else {
+                    server.send404Response(res, ip, request);
+                }
+
+            return true;
+        });
+
+        server.start();
     }
 }
